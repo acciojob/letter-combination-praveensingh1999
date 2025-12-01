428cfb69a643c3c7b1637c14914e625bbb2c2c32
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-12-01 21:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	 if (!input_digit || input_digit.length === 0) return [];
+
+  const map = {
+    "0": "0",
+    "1": "1",
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz"
+  };
+
+  let result = [""];
+
+  for (let digit of input_digit) {
+    const letters = map[digit];
+    const temp = [];
+
+    for (let combo of result) {
+      for (let ch of letters) {
+        temp.push(combo + ch);
+      }
+    }
+
+    result = temp;
+  }
+
+  return result.sort();
 }
 
 module.exports = letterCombinations;
